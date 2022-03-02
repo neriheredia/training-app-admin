bd2f6b65e668bc0ce7812676132a7f7a8aac159a
IgnacioCava <cavaignacio11@gmail.com>
2022-03-02 12:12:04 -0300

    x

--- src/redux/apiCalls/getMonthlyUsers/getMonthlyUsers.js
+++ src/redux/apiCalls/getMonthlyUsers/getMonthlyUsers.js
@@ -7,10 +7,10 @@ import { privateRequest } from '../../../config/requestMethod/privateRequest'
 
 
 //GET MONTHLY STATS
-export const getMonthlyStats = async (dispatch) => {
+export const getMonthlyStats = async (dispatch, type) => {
     dispatch(getMonthlyStatsStart())
     try {
-        const res = await privateRequest.get('admin/monthlyUsers')
+        const res = await privateRequest.get(`admin/stats/${type}`)
         dispatch(getMonthlyStatsSuccess(res.data))
     } catch (err) {
         dispatch(getMonthlyStatsFailure())
